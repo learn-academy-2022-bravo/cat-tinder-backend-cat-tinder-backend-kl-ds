be4e2ce89abedf18fd52bcf6f4f255ba90095830
kmmontes <kmontes4989@gmail.com>
2022-05-13 10:55:28 -0700

    created passing tests to validate :name and :age

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,3 @@
 class Dog < ApplicationRecord
+    validates :name, :age, presence: true
 end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate a name" do
+    dog = Dog.create
+    expect(dog.errors[:name]).to_not be_empty
+  end
+  it "should validate a age" do
+    dog = Dog.create
+    expect(dog.errors[:age]).to_not be_empty
+  end
 end
